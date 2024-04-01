645d2fbbd91e0bfcf24e535f17d643cd15f43bea
Konstantinos Psaroulakis <Konstantinos.Psaroulakis@newcrosshealthcare.com>
2024-04-01 15:00:48 +0300

    added timer_v2 with output file support

--- python/decorators.py
+++ python/decorators.py
@@ -9,3 +9,20 @@ def timer_decorator(func):
         print(f"Execution time: {execution_time} seconds")
         return result
     return wrapper
+
+
+def timer_decorator_v2(output='stdout'):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            start_time = time.time()
+            result = func(*args, **kwargs)
+            end_time = time.time()
+            execution_time = end_time - start_time
+            if output == 'stdout':
+                print(f"Execution time: {execution_time} seconds")
+            elif output.endswith('.txt') or output.endswith('.log'):
+                with open(output, 'a') as file:
+                    file.write(f"Execution time: {execution_time} seconds\n")
+            return result
+        return wrapper
+    return decorator
--- python/test_decorators.py
+++ python/test_decorators.py
@@ -1,6 +1,7 @@
-from decorators import timer_decorator
+from decorators import *
 import time
 
+print("Testing timer_decorator()")
 @timer_decorator
 def test_func1():
     time.sleep(1)
@@ -8,6 +9,36 @@ def test_func1():
 test_func1()  # Exec time: ~1 second
 
 @timer_decorator
+def test_func2(a, b):
+    time.sleep(2)
+    return a + b
+
+result = test_func2(3, 4)
+print(result)  # Result: 7 Exec time: ~2 seconds
+
+print("Testing timer_decorator_v2() with stdout")
+@timer_decorator_v2()
+def test_func1():
+    time.sleep(1)
+
+test_func1()  # Exec time: ~1 second
+
+@timer_decorator_v2()
+def test_func2(a, b):
+    time.sleep(2)
+    return a + b
+
+result = test_func2(3, 4)
+print(result)  # Result: 7 Exec time: ~2 seconds
+
+print("Testing timer_decorator_v2() with file")
+@timer_decorator_v2(output='test_out.txt')
+def test_func1():
+    time.sleep(1)
+
+test_func1()  # Exec time: ~1 second
+
+@timer_decorator_v2('test_out.log')
 def test_func2(a, b):
     time.sleep(2)
     return a + b
