1bb60b8362fb0216bfe9d200b613b5f2417975a8
Konstantinos Psaroulakis <Konstantinos.Psaroulakis@newcrosshealthcare.com>
2024-05-16 13:29:03 +0300

    use pytest mocker in timer testing ++ add given/when/then comments

--- python/test_decorators.py
+++ python/test_decorators.py
@@ -1,4 +1,7 @@
 from decorators import timer_v3
+import sys
+
+
 class MockWritable:
     # This can be any class that implements at least the write method (writable)
     def __init__(self):
@@ -8,14 +11,17 @@ class MockWritable:
         self.outputs.append(s)
 
 def test_timer_v3_with_Writable():
+    # Given 
     mock_output = MockWritable()
 
     @timer_v3(output=mock_output)
     def dummy_function():
         return "result"
 
+    # When
     result = dummy_function()
     
+    # Then
     assert result == "result"
     assert len(mock_output.outputs) == 1
     assert "Execution time:" in mock_output.outputs[0] and "seconds" in mock_output.outputs[0]
@@ -30,5 +36,20 @@ def test_timer_v3_with_non_Writable(capsys):
     captured = capsys.readouterr()
     assert result == "result"
     assert "Given output does not have a write method." in captured.out
-    import pdb; pdb.set_trace()
     assert "Execution time:" in captured.out and "seconds" in captured.out
+
+def test_timer_v3_with_mock_write(mocker):
+    # Given
+    mock_write = mocker.patch('sys.stdout.write')
+
+    @timer_v3(output=sys.stdout)
+    def dummy_function():
+        return "result"
+
+    # When
+    result = dummy_function()
+
+    # Then
+    mock_write.assert_called_once()
+    assert result == "result"
+    assert "Execution time:" in mock_write.call_args_list[0][0][0] and "seconds" in mock_write.call_args_list[0][0][0]
